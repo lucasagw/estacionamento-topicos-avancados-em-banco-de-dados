d4c6e79cfd75c82934f3d5faf034fa70e38b7326
lucas <lucas@DESKTOP-IVOEIAK>
2022-05-20 10:56:54 -0300

    Muda a versão do spring para 2.6.6, e java 11.

--- ucsalAPIGateway/src/main/java/app/ucsal/apigateway/UcsalApiGatewayApplication.java
+++ ucsalAPIGateway/src/main/java/app/ucsal/apigateway/UcsalApiGatewayApplication.java
@@ -2,11 +2,13 @@ package app.ucsal.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class UcsalApiGatewayApplication {
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		SpringApplication.run(UcsalApiGatewayApplication.class, args);
 	}
 }
